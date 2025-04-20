f4b372ce454b15730056443682f776537fc6dac8
Reza nazari <rezanazari.phone@gmail.com>
2025-04-20 20:15:12 +0330

    S16

---
+++ S16/hw/s16con/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace s16con;
+
+class Program
+{
+    static void Print_teacher(Teacher[] cs)
+    {
+        foreach(var c in cs)
+            Console.WriteLine(c.ToString());
+    }
+    static void Main(string[] args)
+    {
+        Teacher jadid = new Teacher("Iliya","hosseini_zade",555666,true,1613339909,360000000,20,97389343);
+        Print_teacher([jadid]);
+    }
+}
