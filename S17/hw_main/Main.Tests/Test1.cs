b64153c08a95eca98a7b2f9e4ed7d0226b7a7bc5
Reza nazari <rezanazari.phone@gmail.com>
2025-04-25 16:38:08 +0330

    S17

---
+++ S17/hw_main/Main.Tests/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Main.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ S17/hw_main/Main/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Main;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Triangle t1 = new Triangle(new Vector(1, 1),new Vector(4, 1),new Vector(2, 5));
+
+        t1.Draw();
+        Console.WriteLine($"S = {t1.GetArea()}");
+    }
+}
