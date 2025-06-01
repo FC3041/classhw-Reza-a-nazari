41cd29b28d9c0d1331912f1eac319da003ad6cbd
Reza-a-nazari <rezanazari.phone@gmail.com>
2025-06-01 20:44:16 +0330

    S28 Atm

---
+++ S28/class/S28console/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace S28console;
+// using System.Windows.Forms;
+using System;
+class Program
+{
+    // // static void Main(string[] args)
+    // // {
+    // //     Console.WriteLine(" launch call of duty warzone ...");
+
+    // //     Thread.Sleep(2000);
+
+    // //     Console.WriteLine("cracked version!");
+
+    // //     Application.EnableVisualStyles();
+
+    // //     Application.Run(new Form());
+    // // }
+
+    // static void Main(string[] args)
+    // {
+
+    // }
+    
+
+
+
+
+}
